6f035a1d0bd72e1491499b18982f19264cb4b8d0
Melanie Seltzer <melleh11@gmail.com>
2018-03-01 09:47:25 -0800

    Modify Atom config files and the install script

--- atom/README.md
+++ atom/README.md
@@ -3,3 +3,7 @@
 You can use the `apm star --installed` command to easily star all the packages you currently have installed.
 
 Then, install all of your starred packages at any time by executing the command `apm stars --install` at the command line.
+
+To create a list of current packages, use the following command in your dotfiles dir:
+
+`apm list --bare --installed --dev false > atom/packages.txt`
--- install.sh
+++ install.sh
@@ -20,24 +20,23 @@ ln -sf "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -sf "$DOTFILES_DIR/zsh/.zshrc" ~
 if [[ ! -d ~/.atom ]]; then mkdir ~/.atom/; fi
 ln -sf "$DOTFILES_DIR/atom/config.cson" ~/.atom/
+ln -sf "$DOTFILES_DIR/atom/keymap.cson" ~/.atom/
 ln -sf "$DOTFILES_DIR/atom/styles.less" ~/.atom/
 
 ####################
 # Atom
 ####################
-# To create a list of current packages, use the following command:
-# apm list --bare --installed --dev false > packages.txt
 
 echo "Starting Atom..."
+
+
 
 # Install packages based on packages.txt
 # Workaround to skip installation if first package exists
 # See https://github.com/atom/apm/issues/170
 if [[ ! -d "$HOME/.atom/packages/atom-beautify" ]]; then
 	if [ -x "$(command -v apm)" ]; then
-		apm install --packages-file $DOTFILES_DIR/atom/packages.txt
-	elif [ -x "$(command -v apm-beta)" ]; then		# Workaround for `apm` vs `apm-beta` binary
-		apm-beta install --packages-file $DOTFILES_DIR/atom/packages.txt
+		apm stars --install && apm list --bare --installed --dev false > $DOTFILES_DIR/atom/packages.txt && apm install --packages-file $DOTFILES_DIR/atom/packages.txt
 	else
 		echo "Atom not installed, skipping."
 	fi
