856bbf2bb811ea90cea0eaf96c7c51780241719f
Melanie Seltzer <melleh11@gmail.com>
2018-05-21 10:10:11 -0700

    Remove Atom refs, fix syntax highlighting plugin

--- install.sh
+++ install.sh
@@ -13,28 +13,5 @@ ln -sf "$DOTFILES_DIR/.gitconfig" ~
 ln -sf "$DOTFILES_DIR/.gitignore_global" ~
 ln -sf "$DOTFILES_DIR/.zshrc" ~
 ln -sf "$DOTFILES_DIR/.hyper.js" ~
-if [[ ! -d ~/.atom ]]; then mkdir ~/.atom/; fi
-ln -sf "$DOTFILES_DIR/atom/config.cson" ~/.atom/
-ln -sf "$DOTFILES_DIR/atom/keymap.cson" ~/.atom/
-ln -sf "$DOTFILES_DIR/atom/styles.less" ~/.atom/
-
-####################
-# Atom
-####################
-
-echo "Starting Atom..."
-
-# Install packages based on packages.txt
-# Workaround to skip installation if first package exists
-# See https://github.com/atom/apm/issues/170
-if [[ ! -d "$HOME/.atom/packages/atom-beautify" ]]; then
-	if [ -x "$(command -v apm)" ]; then
-		apm stars --install && apm list --bare --installed --dev false > $DOTFILES_DIR/atom/packages.txt && apm install --packages-file $DOTFILES_DIR/atom/packages.txt
-	else
-		echo "Atom not installed, skipping."
-	fi
-else
-	echo "Atom packages already installed, skipping."
-fi
 
 echo "Installation complete..."
