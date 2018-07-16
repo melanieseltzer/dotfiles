8cbad6322e76f9d80986d8d8cf1917f953fb5965
Melanie Seltzer <melleh11@gmail.com>
2018-07-16 15:14:53 -0700

    Modify .eslintrc

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ echo "Starting install..."
 
 echo "Starting Symlinks..."
 
-ln -sf "$DOTFILES_DIR/.eslintrc" ~
 ln -sf "$DOTFILES_DIR/.gitconfig" ~
 ln -sf "$DOTFILES_DIR/.gitignore_global" ~
 ln -sf "$DOTFILES_DIR/.zshrc" ~
